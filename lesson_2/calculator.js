26f81c85068633a5f079ffc6996617e97e505706
Khaled Ali <alikhal02@gmail.com>
2022-02-10 10:32:43 -0500

    Finished calculator bonus features and car loan calculator

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -5,6 +5,9 @@
 // Print the result to the terminal.
 
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
+require('./calculator_messages.json');
+
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -14,48 +17,61 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt("Welcome to Calculator!");
+let playAgain = "";
 
-prompt("What's the first number? ");
-let num1 = readline.question();
+prompt(MESSAGES['welcome']);
 
-while (invalidNumber(num1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  num1 = readline.question();
+while (true) {
+
+  prompt("What's the first number? ");
+  let num1 = readline.question();
+
+  while (invalidNumber(num1)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    num1 = readline.question();
+  }
+
+
+  prompt("What's the second number? ");
+  let num2 = readline.question();
+
+  while (invalidNumber(num2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    num2 = readline.question();
+  }
+
+  prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+  let operation = Number(readline.question());
+
+  while (![1, 2, 3, 4].includes(operation)) {
+    prompt('Must choose 1, 2, 3 or 4');
+    operation = Number(readline.question());
+  }
+  console.log(operation);
+  let output;
+  switch (operation) {
+    case 1:
+      output = Number(num1) + Number(num2);
+      break;
+    case 2:
+      output = Number(num1) - Number(num2);
+      break;
+    case 3:
+      output = Number(num1) * Number(num2);
+      break;
+    case 4:
+      output = Number(num1) / Number(num2);
+      break;
+  }
+
+  prompt(`The result is: ${output}`);
+
+  prompt("Would you like to play again?: ('y', or 'yes' for yes.. anything else is no)");
+  playAgain = readline.question();
+  if (playAgain !== 'y' || playAgain !== 'yes') {
+    break;
+  }
 }
 
-
-prompt("What's the second number? ");
-let num2 = readline.question();
-
-while (invalidNumber(num2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  num2 = readline.question();
-}
-
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = Number(readline.question());
-
-while (![1, 2, 3, 4].includes(operation)) {
-  prompt('Must choose 1, 2, 3 or 4');
-  operation = Number(readline.question());
-}
-console.log(operation);
-let output;
-switch (operation) {
-  case 1:
-    output = Number(num1) + Number(num2);
-    break;
-  case 2:
-    output = Number(num1) - Number(num2);
-    break;
-  case 3:
-    output = Number(num1) * Number(num2);
-    break;
-  case 4:
-    output = Number(num1) / Number(num2);
-    break;
-}
-
-prompt(`The result is: ${output}`);
+prompt("Thanks for playing! Goodbye!!");
 
